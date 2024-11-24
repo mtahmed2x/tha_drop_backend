d9737f349751cfe1c783d96016d6a614b70cdcd0
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2024-11-24 12:10:30 +0600

    rewrite

--- src/routers/authRouters.ts
+++
@@ -1,9 +0,0 @@
-import AuthController from "@controllers/authController";
-import express from "express";
-const authRouter = express.Router();
-
-authRouter.post("/register", AuthController.register);
-authRouter.post("/activate", AuthController.activate);
-authRouter.post("/login", AuthController.login);
-
-export default authRouter;
